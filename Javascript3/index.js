cc4881813ef8de2310c4151977054c6657cb7570
azamat09912 <maratazamat09@gmail.com>
2024-10-31 00:09:59 +0500

    Add files via upload

---
+++ Javascript3/index.js
@@ -0,0 +1,94 @@
+/* var name= 'Abdurakhman'
+
+var ernar;
+
+console.log("Ernardyn mani: " + ernar);
+
+
+var name = 25
+
+var durys = true;
+var qate = false;
+
+var natizhe = null; //мани бос
+console.log("Natizhe: " + natizhe);
+
+var san1 = 10
+var san2 = 15
+
+natizhe = san1 + san2 // 10 + 15
+console.log("Бақжаннын нәтижесі:  " + natizhe);
+
+
+if(5 + 5 == 7){
+    console.log(durys);
+}
+else{
+    console.log(qate);    
+}
+
+// const name = "Bakzhan"
+
+let Bakzhan = 5;
+
+if(true){
+    let Bakzhan=10;
+    console.log("a: " + Bakzhan);
+    
+}
+console.log("a: " + Bakzhan);
+
+
+const text= "Сіздің паролиниз немесе логин кате"
+
+const PI = 3.14;
+
+PI=2.15
+
+// PI= 3.25
+
+// name = "Galymbek"
+
+console.log("PI: " + PI);
+ */
+var name= 'azamat'
+
+var azamat;
+
+console.log("azamat mani: " + azamat);
+
+console.log(typeof name); //string
+
+var age= 14  
+
+console.log("age mani: " + 14);
+
+console.log(typeof age); //number
+
+ var age= toString("15")
+
+ 
+console.log(typeof name); //string
+
+
+var durys = true;
+var qate = false;
+
+var san1 = 10
+var san2 = 15
+
+natizhe = san1 + san2 // 10 + 15
+console.log("Азаматың нәтижесі:  " + natizhe);
+
+
+if(10 + 15 == 25){
+    console.log(durys);
+}
+else{
+    console.log(qate);    
+}
+console.log(typeof Boolean); //Buoleon
+
+
+
+
